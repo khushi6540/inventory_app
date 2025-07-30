6ed2baac937b1c8fe4c2db8c7bcf446334377502
Khushi Kumari <kumarikhushi6540@gmail.com>
2025-07-30 21:43:25 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 # app.py
 import streamlit as st
 import pandas as pd
-import plotly.express as px
+from plotly import express as px
 from utils import load_model, preprocess_input
 
 st.set_page_config(page_title="Inventory Management Dashboard", layout="wide")
